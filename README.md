6e65d020a7a8b5b87bca2587228459163a705d14
everpointer <vane.zhangfeng@gmail.com>
2015-05-08 16:33:00 +0800

    Add README.md to descrbe the whole project and add comment to config.php

--- README.md
+++ README.md
@@ -1,30 +1,80 @@
+# 支付宝服务窗会员模块
+此模块可独立做为一个web应用，绑定在服务窗菜单上使用，也可以与现有服务窗代码集成，作为其一部分。
 
-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
-    ----------------------------------------------------------------- 
+## 功能
+- 老会员绑定
+	使用手机号和密码等会员信息，验证后绑定到当前支付宝账户
+- 新会员购买
+	支付宝购买，支付成功后，注册当前支付宝会员账号
+- 显示会员账号信息
+	显示会员卡号，用于门店出示使用
 
+## 安装说明（依赖）
+### 运行环境
+- 开发语言
+	PHP, 版本 \>= 5.4 （低版本未测试过，开发版本为5.4）
+- 模块依赖
+	- php extensions: php\_curl.so
+		支持REST API调用
+	- composer packages 
+		pingpp-php:  支付网关库，支付宝支付接口的中间件
+		**composer.json:**
+	{
+	  "require": {
+	    "pingplusplus/pingpp-php": "dev-master"
+	  }
+	}
+## 模块配置
+- 配置文件 : config.php
 
-Hi there! Welcome to Cloud9 IDE!
+### 会员接口说明 
+Todo:
+## 目录结构
+- GYMember : 项目目录
+	-  config.php : 模块基本配置，API接口设置
 
-To get you started, we have created a small hello world application.
+	- aop
+	- lotusphp\_runtime
+	- AlipaySign.php
+	- AopSdk.php
+	- Gateway.php
+	- HttpRequest.php
+		支付宝服务窗SDK，用于调用支付宝OAuth等接口。单独项目需要包含这些文件，若集成当前服务窗项目，则应该已自带。
 
-1) Open the hello-world.php file
+	-  key
+		- alipay\_rsa\_public\_key.pem
+		- rsa\_private\_key.pem
+		- rsa\_public\_key.pem
+		rsa密钥文件，服务窗，支付宝手机网站支付和ping++等接口的配置，需要上述密钥信息。密钥生成方式，参考附录。
 
-2) Follow the run instructions in the file's comments
+	- assets
+		- css
+		- image
+		- js
+			- pingpp\_pay.js : ping++ Html客户端库
+		会员展示和购买等页面，需要用到的资源
 
-3) If you want to look at the Apache logs, check out ~/lib/apache2/log
+	- src
+		- Rest.php : Rest API基础类（基于curl）
+		- Api.php : config 里的接口调用实用类（基于Rest.php)
+		- Member.php : 会员API封装类（基于Api.php）
+	- autoload.php : 自动加载src目录里的类
+	- function.inc.php : 常用方法库
+	- common.php : 通用文件，包含常用方法库，设置支付宝用户ID等
+	- composer.json : php包依赖管理文件
 
-And that's all there is to it! Just have fun. Go ahead and edit the code, 
-or add new files. It's all up to you! 
+	- index.php : 会员中心首页
+	- member
+		- show.php : 会员展示页
+	- bindMember.php : 会员绑定显示页
+	- doBindMember.php : 会员绑定处理页
+	- purchase.php : 会员购买页
+	- pingpp.php : 会员购买创建支付页（通过ping++, 生成支付宝支付页面）
+	- notifyFinishPurchase.php : 支付完成异步通知页
+	- finishPurchase.php : 支付完成返回显示页（会员注册页）
+	- cancelPurchase.php : 支付取消或失败的返回显示页
+	- registerMember.php : 会员注册处理页（购买成功后）
 
-Happy coding!
-The Cloud9 IDE team
-
-
-## Support & Documentation
-
-Visit http://docs.c9.io for support, or to learn more about using Cloud9 IDE. 
-To watch some training videos, visit http://www.youtube.com/user/c9ide
+	- UserInfo.php : 支付宝会员信息接口类
+	- .gitignore
+	- README.md : 模块说明页
--- config.php
+++ config.php
@@ -1,59 +1,66 @@
 <?php
 return array(
-  'alipay_public_key_file' => dirname ( __FILE__ ) . "/key/alipay_rsa_public_key.pem",
-	'merchant_private_key_file' => dirname ( __FILE__ ) . "/key/rsa_private_key.pem",
-	'merchant_public_key_file' => dirname ( __FILE__ ) . "/key/rsa_public_key.pem",		
-	'charset' => "GBK",
-	'gatewayUrl' => "https://openapi.alipay.com/gateway.do",
-	'app_id' => "2015031700036703",
-	'partner_id' => "2088311515754978",
-	'partner_name' => "果忆",
-	'card_price' => 10,  // 单位元
-	'pingxx' => array(
-  	'app_id' => 'app_unXbrDnvDaj90O4e',
-  	'test_key' => 'sk_test_1i1abHv5mbjHHCuTKCDyXP08',
-  	'live_key' => 'sk_live_wULlucfQQ6k4bEYjp6k8r2h9',
-  	'env_key' => 'live_key'
+  'alipay_public_key_file' => dirname ( __FILE__ ) . "/key/alipay_rsa_public_key.pem", // 支付宝公钥文件路径
+	'merchant_private_key_file' => dirname ( __FILE__ ) . "/key/rsa_private_key.pem", // 商户私钥文件路径
+	'merchant_public_key_file' => dirname ( __FILE__ ) . "/key/rsa_public_key.pem",	// 商户公钥文件路径
+	'charset' => "GBK", // 字符集，支付宝接口需要GBK，无需改变
+	'gatewayUrl' => "https://openapi.alipay.com/gateway.do", // 支付宝接口网关地址, 无需改变
+	'app_id' => "2015031700036703", // 服务窗app id, 服务窗后台查看
+	'partner_id' => "2088311515754978", // 商户partner id，支付宝后台查看
+	'partner_name' => "果忆", // 商户名称
+	'card_price' => 10,  // 会员卡单价，单位元
+	'pingxx' => array( // ping++ 配置, ping++后台可查看
+  	'app_id' => 'app_unXbrDnvDaj90O4e',  // ping++ app_id
+  	'test_key' => 'sk_test_1i1abHv5mbjHHCuTKCDyXP08', // ping++ 测试环境key
+  	'live_key' => 'sk_live_wULlucfQQ6k4bEYjp6k8r2h9', // ping++ 正式环境key
+  	'env_key' => 'live_key' // 当前使用环境, 正式环境才可调用支付宝
 	 ),
-  'header' => array(
+  'header' => array( // 调用Rest API时，需要额外传入的请求头部信息, 没有就保持空
     'X-AVOSCloud-Application-Id: 0s4hffciblz94hah0m63rsn0x970m2obrjthz0cwmqwsipdy',
-    'X-AVOSCloud-Application-Key: 0b7jsd5h44y4wcv6w4w0alomwmpwufx8odk3irmvk36q2g10'  
+    'X-AVOSCloud-Application-Key: 0b7jsd5h44y4wcv6w4w0alomwmpwufx8odk3irmvk36q2g10'
   ),
-  'api' => array(
-    'bind' => array(
-      'method' => 'update',
-      'url' => 'https://leancloud.cn/1.1/login',
-      'params' => array('mobile', 'password', 'uid')
-    ),
+  'api' => array( // 接口列表，配置对应的url, params 和 optionParams
       /**
-       * @return Member return a member object with member card ID status code: 200 means a
-       * successful request.
-       */ 
+       * 新会员注册接口
+       *
+       */
     'register' => array(
       'method' => 'post',
       'url' => 'https://api.leancloud.cn/1.1/classes/Member',
       'params' => array('mobile', 'password', 'uid')
     ),
+    /**
+     * 会员信息查询接口，支持通过支付宝用户ID 或 手机号和密码 来查询用户
+     *
+     */
     'getMemberInfo' => array(
       'method' => 'get',
       'url' => 'https://api.leancloud.cn/1.1/classes/Member',
       'params' => array(),
       'optionParams' => array('uid', 'where', 'mobile', 'password')
     ),
+    /**
+     * 老会员绑定支付宝用户接口
+     *
+     */
     'updateMemberInfo' => array(
       'method' => 'put',
       'url' => 'https://api.leancloud.cn/1.1/classes/Member/%s',
       'params' => array('uid')
     ),
       /**
-       * @return string return a string orderId and status code: 200 means a
-       * successful request.
-       */ 
+       * 会员卡购买订单创建接口
+       *
+       */
     'createCardOrder' => array(
       'method' => 'post',
       'url' => 'https://api.leancloud.cn/1.1/classes/CardOrder',
       'params' => array('uid', 'amount', 'paid', 'binded')
     ),
+    /**
+     * 会员卡购买订单更新接口
+     *
+     */
     'updateCardOrder' => array(
       'method' => 'put',
       'url' => 'https://api.leancloud.cn/1.1/classes/CardOrder/%s',
@@ -61,6 +68,7 @@ return array(
       'optionParams' => array('outTradeNo', 'paid', 'binded', 'status')
     ),
       /**
+       * 会员卡订单查询接口
        * @return CardOrder return one user card order code: 200 means a
        * successful request.
        */ 
@@ -70,6 +78,10 @@ return array(
       'params' => array(),
       'optionParams' => array('uid', 'orderId', 'where')
     ),
+    /**
+     * 会员卡订单删除接口（或取消接口）
+     *
+     */
     'deleteCardOrder' => array(
       'method' => 'delete',
       'url'    => 'https://api.leancloud.cn/1.1/classes/CardOrder/%s',
