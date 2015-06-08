b3b0c6336a7a90949ae14b930183945cb4052d67
everpointer <vane.zhangfeng@gmail.com>
2015-06-08 16:50:19 +0000

    add timezone setting

--- common.php
+++ common.php
@@ -1,4 +1,7 @@
 <?php
+// 设置时区
+date_default_timezone_set("PRC");
+
 require_once 'Api.php';
 require_once 'function.inc.php';
 require_once 'UserInfo.php';
@@ -6,8 +9,8 @@ require_once 'UserInfo.php';
 $api = new \LyfMember\Api();
 $config = (require 'config.php');
 $userinfo = new UserInfo ();
-$uid = $userinfo->getUserId ();
-// $uid = '20881016718708634955964451718217';
+// $uid = $userinfo->getUserId ();
+$uid = '20881016718708634955964451718217';
 if (!$uid && isset( $_REQUEST['auth_code'] )) {
   $uid = $userinfo->getUserId ( $_REQUEST['auth_code'] );
 } else if (!$uid) {
