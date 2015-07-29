cba11029d2b814ebeec15ff53049b3560d0e7bbd
everpointer <vane.zhangfeng@gmail.com>
2015-07-29 03:28:35 +0000

    add AVConfig.php to repo

---
+++ sdk/leancloud/AVConfig.php
@@ -0,0 +1,10 @@
+<?php namespace leancloud;
+
+return array(
+  'app_id' => getenv('leancloud_app_id'),
+  'app_key' => getenv('leancloud_app_key'),
+  'master_key' => getenv('leancloud_master_key'),
+  'leancloud_url' => 'https://leancloud.cn/1.1/'
+);
+
+?>
