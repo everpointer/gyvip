edc8f29caf5a9080659d3dea539810a6eac62316
everpointer <vane.zhangfeng@gmail.com>
2015-12-01 04:10:06 +0000

    remove credit/index.php comment

--- credit/index.php
+++ credit/index.php
@@ -11,24 +11,24 @@ if (!isset($memberInfo)) {
   exit;
 }
 // // query member balance
-// $api = new LyfMember\Api();
-// // 查询用户积分余额
-// $apiParams = array(
-//   'name' => $memberInfo['cardNumber'],
-//   'userType' => 2, // member card
-//   'accountType' => 3, // 积分余额
-//   'businessId' => 1,
-// );
-// $apiParams['sign'] = genKmtkApiSign($apiParams);
-// $resultStr = $api->call('kmtkBalance', $apiParams);
-// $result = json_decode($resultStr);
+$api = new LyfMember\Api();
+// 查询用户积分余额
+$apiParams = array(
+  'name' => $memberInfo['cardNumber'],
+  'userType' => 2, // member card
+  'accountType' => 3, // 积分余额
+  'businessId' => 1,
+);
+$apiParams['sign'] = genKmtkApiSign($apiParams);
+$resultStr = $api->call('kmtkBalance', $apiParams);
+$result = json_decode($resultStr);
 
-// if(!$result || isset($result->data->error)) {
-//   header($_SERVER["SERVER_PROTOCOL"]." 501 Bad Request"); 
-//   exit();
-// } else if (!empty($result->message)) {
-//   die("发生错误：$result->message");
-// }
+if(!$result || isset($result->data->error)) {
+  header($_SERVER["SERVER_PROTOCOL"]." 501 Bad Request"); 
+  exit();
+} else if (!empty($result->message)) {
+  die("发生错误：$result->message");
+}
 
 // generate duiba page url, url的有效期为5分钟，保证安全
 $duibaUid = $memberInfo['cardNumber'];  // 会员卡号为用户标识，统一支付宝和微信
