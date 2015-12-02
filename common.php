e8ef2d6f4d5936fe0514b7919251f772cd18fa57
everpointer <vane.zhangfeng@gmail.com>
2015-12-02 05:57:30 +0000

    add oauth proxy_redirect support for other wechat platform

--- common.php
+++ common.php
@@ -32,21 +32,33 @@ if (isset($_SESSION['uid'])) { // other pages
     $oauth = new \Henter\WeChat\OAuth($config['wechat']['app_id'], $config['wechat']['app_secret']);
     if (!isset($_GET['code'])) {
       $callback_url = $config['host'] . "?platform=$platform";
+      // url入口格式：host?platform=wechat&proxy_redirect
+      // 通过proxy_redirect实现转交code和重定向
+      if ($_GET['proxy_redirect']) {
+        $callback_url = $callback_url . "&proxy_redirect=" . $_GET['proxy_redirect'];
+      }
       $url = $oauth->getWeChatAuthorizeURL($callback_url, 'snsapi_userinfo');
       header("Location: $url");
       exit;
     } else {
       $code = $_GET['code'];
-      if($access_token = $oauth->getAccessToken('code', $code)){
-        $refresh_token = $oauth->getRefreshToken();
-        $expires_in = $oauth->getExpiresIn();
-        $openid = $oauth->getOpenid();
-        $_SESSION['uid'] = $openid;
-        $_SESSION['platform'] = $platform;
-        $uid = $openid;
-      }else{
-        // echo $oauth->error();
-        exit("User fail to authorized!");
+      // 转交微信auth code实现统一授权 for weiqing
+      if (isset($_GET['proxy_redirect'])) {
+        $proxy_redirect_url = $_GET['proxy_redirect'] . "&code=" . $code;
+        header("Location: $proxy_redirect_url");
+        exit;
+      } else {
+        if($access_token = $oauth->getAccessToken('code', $code)){
+          $refresh_token = $oauth->getRefreshToken();
+          $expires_in = $oauth->getExpiresIn();
+          $openid = $oauth->getOpenid();
+          $_SESSION['uid'] = $openid;
+          $_SESSION['platform'] = $platform;
+          $uid = $openid;
+        }else{
+          // echo $oauth->error();
+          exit("User fail to authorized!");
+        }
       }
     }
   }
